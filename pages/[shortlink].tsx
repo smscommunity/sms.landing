203899c66f84301794427bc99a532efc803323c4
Theo <zeldocto@gmail.com>
2025-07-25 23:24:27 -0700

    Update [shortlink].tsx

--- pages/[shortlink].tsx
+++ pages/[shortlink].tsx
@@ -23,7 +23,7 @@ export default function ShortLink(props: ShortLinkProps) {
 // 1. Edit shortLinks to contain your new short link slug.
 // 2. Update getHref to add to the case/switch the new link you want to shortlink to.
 // 3. You are done!
-const shortLinks = ['discord', 'il', 'wiki', 'rta', 'ilrules', 'twitch', 'yt'] as const;
+const shortLinks = ['discord', 'il', 'wiki', 'rta', 'ilrules', 'twitch', 'yt', 'comsob'] as const;
 type ShortLinks = typeof shortLinks[number];
 
 export const getStaticProps: GetStaticProps = ({ params }) => {
@@ -51,6 +51,8 @@ function getHref(shortLinkTarget: ShortLinks): string {
             return 'https://www.twitch.tv/sunshinecommunity'       
         case 'yt':
             return 'https://www.youtube.com/smscommunity'
+        case 'comsob':
+            return 'https://docs.google.com/spreadsheets/d/12wDUXjLqmcUuWSEXWc1fHNJc24KlfyCh0pvibZYEQM0/edit?gid=1529893617#gid=1529893617&range=A1'
     }
 }
 
